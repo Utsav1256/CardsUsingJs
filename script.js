e45956d257f3e4682bb3c2348a7c6c219e2b2800
utsav kumar <utsavkumar5462@gmail.com>
2024-01-04 16:14:00 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+const users = [
+  {
+    name: "Utsav Kumar",
+    image:
+      "https://images.unsplash.com/photo-1512236393941-3d6da97e56bc?q=80&w=871&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    status: "Stranger",
+  },
+  {
+    name: "Priti Kumari",
+    image:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    status: "Stranger",
+  },
+  {
+    name: "Rohan Kumar",
+    image:
+      "https://images.unsplash.com/photo-1500048993953-d23a436266cf?q=80&w=869&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+    status: "Stranger",
+  },
+];
+
+let clutter = "";
+users.forEach((user) => {
+  clutter += `<div id="card">
+  <div id="img"><img src="${user.image}"></img></div>
+  <div id="name">
+  <h3>${user.name}</h3>
+  </div>
+  <div id="description">
+  Lorem ipsum dolor sit amet consectetur adipisicing elit. Unde, odit!
+  </div>
+  <div id="status">${user.status}</div>
+  <button>Add Friend</button>
+  </div>`;
+});
+
+document.querySelector("#main").innerHTML = clutter;
